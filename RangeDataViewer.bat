3a0ff43d70c74a578e4f43e95e740d972f52d8d0
y-masutani <masutani@isc.osakac.ac.jp>
2019-04-25 10:45:58 +0900

    起動用バッチファイルを微変更．

--- RangeDataViewer.bat
+++ RangeDataViewer.bat
@@ -6,6 +6,6 @@ for /F %%A in ('dir "%JAR_BASE%OpenRTM*" /B') do (set FILE1=%%A)
 for /F %%A in ('dir "%JAR_BASE%commons-cli*" /B') do (set FILE2=%%A)
 for /F %%A in ('dir "%JAR_BASE%jna-?.?.?.*" /B') do (set FILE3=%%A)
 for /F %%A in ('dir "%JAR_BASE%jna-platform-?.?.?.*" /B') do (set FILE4=%%A)
-set CLASSPATH=.;%JAR_BASE%%FILE1%;%JAR_BASE%%FILE2%;%JAR_BASE%%FILE3%;%JAR_BASE%%FILE4%;%~dp0\bin
+set CLASSPATH=.;%JAR_BASE%%FILE1%;%JAR_BASE%%FILE2%;%JAR_BASE%%FILE3%;%JAR_BASE%%FILE4%;.\bin
 start "%~n0" java %~n0Comp -f rtc.conf %*
 popd
